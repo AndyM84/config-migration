b698b8d01e5f3b1b201d465ec64d1e77b92d8a42
Andrew Male <mamale@outlook.com>
2021-01-21 02:43:46 -0500

    Modifications for PHP 7.4/8 Compatibility

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,10 +3,4 @@
 	setlocale(LC_ALL, 'en_US.utf8');
 	date_default_timezone_set('America/New_York');
 
-	if (extension_loaded('xdebug')) {
-		xdebug_enable();
-	} else {
-		echo("xdebug extension not found, please configure and retry\n");
-	}
-
 	require('./vendor/autoload.php');
